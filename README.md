30500b5d4fe6fada1c91b95c86c4046c477f5edc
jon <shalommmitz@gmail.com>
2019-11-03 08:45:46 +0200

    Intial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+<p align="center">
+  <h3 align="center">Play Audiobook</h3>
+
+  <p align="center">
+    Very simple to operate player of audio books
+  </p>
+</p>
+<br>
+Some persons, especially elderly persons that are visually challenged, need as simple as possible user interface.
+For those persons, this project was created.
+It is assumed that another person puts the mp3 (or other VLC-playable format) audio files under the "book" folder.
+To play the book, you need to touch the screen.
+To pause playing, you need to touch the screen.
+This is it. The whole user interface.
+
+
+## Prerequisites and Environment
+This project was written and tested using Ubuntu 18.04 and Python 2.7
+You will need VLC and:
+
+    sudo apt-get install python-dbus
+
+## Author
+
+**Shalom Mitz** - [shalommmitz](https://github.com/shalommmitz)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE ) file for details.
+
---
+++ vlc_dbus.py
@@ -0,0 +1,92 @@
+import dbus, os, time
+
+def toLog(msg):
+    print msg
+
+class VLC(object):
+    def __init__(self):
+        player_interface     = 'org.mpris.MediaPlayer2.Player'
+        tracklist_interface     = 'org.mpris.MediaPlayer2.TrackList'
+        main_interface         = 'org.mpris.MediaPlayer2'
+
+        self.pid = self.load()
+        addr = "org.mpris.MediaPlayer2.vlc.instance"+ self.pid
+        bus = dbus.SessionBus()
+        vlc_obj = bus.get_object(addr, "/org/mpris/MediaPlayer2")
+        self.main       = dbus.Interface(vlc_obj, dbus_interface=main_interface)
+        self.player     = dbus.Interface(vlc_obj, dbus_interface=player_interface)
+        self.tracklist  = dbus.Interface(vlc_obj, dbus_interface=tracklist_interface)
+        self.prop       = dbus.Interface(vlc_obj, 'org.freedesktop.DBus.Properties')
+        return         
+
+    def load(self):
+        # Do we have a running VLC ?
+        pid = self.get_vlc_pid()
+        if not pid:
+            toLog("Launching VLC")
+            os.system("nohup vlc -I dummy --control dbus &")
+            toLog("waiting 2 seconds")
+            time.sleep(2)
+        # If not, lets try to launch it
+        pid = self.get_vlc_pid()
+        if not pid:
+            toLog("Failed launching VLC - Aborting")
+            exit()
+        return pid
+        
+    def get_vlc_pid(self):
+        return os.popen("pgrep vlc").read().strip()
+
+    def play(self):
+        self.player.Play()
+
+    def pause(self):
+        self.player.Pause()
+
+    def get_status(self):
+        return self.prop.Get('org.mpris.MediaPlayer2.Player', 'PlaybackStatus')
+
+    def get_position(self):
+        return self.prop.Get("org.mpris.MediaPlayer2.Player", "Position")
+   
+    def show_available_methods():
+        props = self.prop.GetAll('org.mpris.MediaPlayer2')
+        print "for root:"
+        for k in props.keys():
+            print "   ", k
+        return
+        print "for player:"
+        props = self.prop.GetAll('org.mpris.MediaPlayer2.Player')
+        for k in props.keys():
+            print "   ", k
+        return
+
+    def get_metadata(self):
+        return self.prop.Get("org.mpris.MediaPlayer2.Player", "Metadata")
+
+    def get_lenght(self):
+        # Return position, in micro seconds, of currently playing track
+        return self.get_metadata()['mpris:length']
+
+    def get_position(self):
+        # Return lenght, in micro seconds, of currently playing track
+        return self.prop.Get('org.mpris.MediaPlayer2.Player', 'Position')
+
+    def add_audio_file(self, fn):
+        # Add audio file to playing list. 'fn' must be full path !
+        obj_no_track = '/org/mpris/MediaPlayer2/TrackList/NoTrack'
+        self.tracklist.AddTrack('file://' + fn, obj_no_track, True)
+
+
+if __name__=="__main__":
+    print "main"
+    vlc = VLC()
+    vlc.add_audio_file("/home/jon/play_audiobook/test.mp3")
+    vlc.play()
+    time.sleep(0.4)
+    for i in range(1000):
+        print "For time passed:", i*0.5
+        print "    status:", vlc.get_status()
+        print "    position:", vlc.get_position()
+        print "    len:", vlc.get_lenght()
+        time.sleep(0.5)
